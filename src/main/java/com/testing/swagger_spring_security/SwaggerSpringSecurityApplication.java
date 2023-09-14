316861fc4903bfba4a40cc61ebaf1d06eb686030
ei_ei_kyaw_97 <39520950+EiEiKyaw@users.noreply.github.com>
2023-09-14 12:09:34 +0630

    initial commit

---
+++ src/main/java/com/testing/swagger_spring_security/SwaggerSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.testing.swagger_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testing/swagger_spring_security/SwaggerSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.swagger_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
